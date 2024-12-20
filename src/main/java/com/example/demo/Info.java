8650effb33769cf30bb1716541c0542533413c9f
priceelio <faroukeyad11@gmail.com>
2024-12-20 18:05:27 +0200

    Updated

--- src/main/java/com/example/demo/Info.java
+++ src/main/java/com/example/demo/Info.java
@@ -1,9 +1,35 @@
 package com.example.demo;
 
+import Project.Payment.Card;
+import Project.Person.Admin;
 import Project.Person.Customer;
+import Project.Person.Delivery_Staff;
 import Project.Resturants.Restaurant;
+import Project.Resturants.Review;
+
+import java.util.List;
 
 class Info {
     static Customer customer;
     static Restaurant restaurant;
+    static List<Restaurant> restaurants;
+    static List<Review>reviewsRestaurant;
+    public void loadData(){
+        Restaurant restaurant = new Restaurant();
+        restaurants = restaurant.loadData();
+        Review rev=new Review();
+        reviewsRestaurant=rev.loadDataReviewRestaurant();
+        Customer customer = new Customer();
+        customer.loadData();
+
+        Admin admin = new Admin();
+        admin.loadData();
+
+
+        Card card = new Card();
+        card.loadData();
+
+        Delivery_Staff deliveryStaff=new Delivery_Staff("shubra");
+        deliveryStaff.loadData();
+    }
 }
